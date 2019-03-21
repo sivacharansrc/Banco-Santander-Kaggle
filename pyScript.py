fb1983bc7435e34ac258db5eef814e505261ea03
sivacharansrc <sivacharansrc@gmail.com>
2019-03-21 01:51:57 -0500

    added gitignore

---
+++ pyScript.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+# IMPORTING THE TRAINING FILE
+df_train = pd.read_csv("C:\\Users\\sivac\\Documents\\Python Projects\\Banco Santander Kaggle\\input\\train.csv")
